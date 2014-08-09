2688f07145e2d5c52a8cac9b881884fa272665b7
Sanjay Joshi <joshisa@us.ibm.com>
2014-08-09 17:25:04 -0400

    Updated Sails Bootstrap and manifest

--- config/bootstrap.js
+++ config/bootstrap.js
@@ -13,5 +13,22 @@ module.exports.bootstrap = function(cb) {
 
   // It's very important to trigger this callback method when you are finished
   // with the bootstrap!  (otherwise your server will never lift, since it's waiting on the bootstrap)
+  // ***
+  // *** Required modules, Listed in the order of importance
+  // ***
+  var OpenTokLibrary = require('opentok');
+
+  // ***
+  // *** OpenTok Constants for creating Session and Token values
+  // ***
+  var OTKEY = process.env.TB_KEY;
+  var OTSECRET = process.env.TB_SECRET;
+
+  // ***
+  // *** Setup when server first starts
+  // ***
+  var urlSessions = {};
+  var OpenTokObject = new OpenTokLibrary.OpenTokSDK(OTKEY, OTSECRET);
+  
   cb();
 };
